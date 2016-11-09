3a1a2c997dcc03ed408b0741a4b2f7759486d146
James Baker <jamesbak@microsoft.com>
2016-11-09 15:00:31 -0800

    Fixed bug in pach start script

--- run_pachyderm.sh
+++ run_pachyderm.sh
@@ -13,6 +13,6 @@ sed -i "s/\[ACCOUNT\]/$account/g" $manifest_file
 sed -i "s/\[CONTAINER\]/$container/g" $manifest_file
 sed -i "s/\[STORAGE-KEY\]/$storagekey/g" $manifest_file
 # Start Pachyderm
-kubectl create -f $manifest
+kubectl create -f $manifest_file
 
 echo "Pachyderm is now running and configured to auto-restart"
