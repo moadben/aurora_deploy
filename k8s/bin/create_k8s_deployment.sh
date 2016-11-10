8b5592e4344e0aec16e871f6eca1268d7a3e911e
jim spring <jmspring@gmail.com>
2016-11-10 10:27:59 -0800

    create k8s config via acs-engine and upload template and parameters

--- k8s/bin/create_k8s_deployment.sh
+++ k8s/bin/create_k8s_deployment.sh
@@ -15,6 +15,7 @@ KUBERNETES_SUBNET="$DNS_PREFIX""KubernetesSubnet"
 GLUSTER_SUBNET="$DNS_PREFIX""GlusterSubnet"
 
 SUBSCRIPTION_ID=`azure account list --json | jq -r '.[0].id'`
+RESOURCE_GROUP_REGION=`azure group show jmstest3 --json | jq -r '.location'`
 
 SSH_KEY=`cat $SSH_KEYFILE`
 
@@ -22,6 +23,7 @@ SCRIPT_DIR=`dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )`
 K8S_CONFIG_FILE=`echo ${SCRIPT_DIR}/k8s/config/kubernetesvnet.json`
 K8S_DEPLOYMENT_FILE=`echo ${SCRIPT_DIR}/k8s/config/kube-acsengine-$NOW.json`
 
+echo "Generating acs-engine config."
 cat $K8S_CONFIG_FILE \
     | sed "s/@@RESOURCE_GROUP@@/$RESOURCE_GROUP/g" \
     | sed "s/@@DNS_PREFIX@@/$DNS_PREFIX/g" \
@@ -36,3 +38,25 @@ cat $K8S_CONFIG_FILE \
     | sed "s~@@SSH_KEY@@~$SSH_KEY~g" \
     | tee $K8S_DEPLOYMENT_FILE
 
+echo "Executing acs-engine to generate K8s config"
+ACS_ENGINE_OUTPUT_DIR=`echo ${SCRIPT_DIR}/k8s/config/output/kube-config-$NOW`
+ACS_ENGING_BIN=`echo ${SCRIPT_DIR}/bin/acs-engine`
+$ACS_ENGING_BIN -artifacts $ACS_ENGINE_OUTPUT_DIR $K8S_DEPLOYMENT_FILE
+
+echo "Setting up storage account for K8s artifacts"
+K8S_STORAGE_ACCOUNT_NAME="$DNS_PREFIX""assetstore"
+K8S_STORAGE_ACCOUNT_CONTAINER="$DNS_PREFIX""$NOW"
+STORAGE_ACCOUNT_AVAILABLE=`azure storage account check --json "$K8S_STORAGE_ACCOUNT_NAME" | jq -r '.nameAvailable'`
+if [ "$STORAGE_ACCOUNT_AVAILABLE" == "true" ]
+then
+    echo "Creating storage account"
+    azure storage account create --location "$RESOURCE_GROUP_REGION" --resource-group "$RESOURCE_GROUP" --sku-name LRS --kind Storage "$K8S_STORAGE_ACCOUNT_NAME"
+fi 
+K8S_STORAGE_ACCOUNT_KEY=`azure storage account keys list --json --resource-group "$RESOURCE_GROUP" "$K8S_STORAGE_ACCOUNT_NAME" | jq -r '.[0].value'`
+azure storage container create --account-name "$K8S_STORAGE_ACCOUNT_NAME" --account-key "$K8S_STORAGE_ACCOUNT_KEY" --permission Blob "$K8S_STORAGE_ACCOUNT_CONTAINER"
+
+echo "Moving K8S deployment assets to storage account"
+K8S_AZUREDEPLOY_JSON=`echo ${ACS_ENGINE_OUTPUT_DIR}/azuredeploy.json`
+K8S_AZUREDEPLOY_PARAMETERS_JSON=`echo ${ACS_ENGINE_OUTPUT_DIR}/azuredeploy.parameters.json`
+azure storage blob upload --json --account-name "$K8S_STORAGE_ACCOUNT_NAME" --account-key "$K8S_STORAGE_ACCOUNT_KEY" --file "$K8S_AZUREDEPLOY_JSON" --blobtype block --container "$K8S_STORAGE_ACCOUNT_CONTAINER" --blob azuredeploy.json
+azure storage blob upload --json --account-name "$K8S_STORAGE_ACCOUNT_NAME" --account-key "$K8S_STORAGE_ACCOUNT_KEY" --file "$K8S_AZUREDEPLOY_PARAMETERS_JSON" --blobtype block --container "$K8S_STORAGE_ACCOUNT_CONTAINER" --blob azuredeploy.parameters.json
