8b3760265d7fbf3d47a5df73b742f58283ad6a91
Brendan Cassidy <brendanacassidy@gmail.com>
2016-11-10 14:46:26 -0800

    changed to my repo so I can test it
    It will be ideal to not have github repos hardcoded

--- ingress/startup.sh
+++ ingress/startup.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/jpoon/aurora_deploy/master/ingress/docker.presence.service > /etc/systemd/system/docker.presence.service
-curl https://raw.githubusercontent.com/jpoon/aurora_deploy/master/ingress/docker.topach.service > /etc/systemd/system/docker.topach.service
+curl https://raw.githubusercontent.com/brendanacassidy/aurora_deploy/master/ingress/docker.presence.service > /etc/systemd/system/docker.presence.service
+curl https://raw.githubusercontent.com/brendanacassidy/aurora_deploy/master/ingress/docker.topach.service > /etc/systemd/system/docker.topach.service
 mkdir /etc/aurora
-curl https://raw.githubusercontent.com/jpoon/aurora_deploy/master/ingress/presence.conf > /etc/aurora/presence.conf
-#echo "USER=$1" >> $conf_file
-#echo "PASSWORD=$2" >> $conf_file
-#echo "PACH_IP=$3" >>$conf_file
-#echo "MONGO_URL=$4" >>$conf_file
-#echo "DATABASE_NAME=$5" >>$conf_file
-#echo "DBCOLLECTION=$6" >> $conf_file
-curl https://raw.githubusercontent.com/jpoon/aurora_deploy/master/ingress/topach.conf > /etc/aurora/topach.conf
+curl https://raw.githubusercontent.com/brendanacassidy/aurora_deploy/master/ingress/presence.conf > /etc/aurora/presence.conf
+curl https://raw.githubusercontent.com/brendanacassidy/aurora_deploy/master/ingress/topach.conf > /etc/aurora/topach.conf
 #echo "USER=$1" >> $conf_file
 #echo "PASSWORD=$2" >> $conf_file
 #echo "PACH_IP=$3" >>$conf_file
