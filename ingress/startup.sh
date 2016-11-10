08c872dcb7922a5869688d1e6f251e61c9f36eb9
Brendan Cassidy <brendanacassidy@gmail.com>
2016-11-10 14:37:24 -0800

    Update startup.sh

--- ingress/startup.sh
+++ ingress/startup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+curl https://raw.githubusercontent.com/jpoon/aurora_deploy/master/ingress/docker.presence.service > /etc/systemd/system/docker.presence.service
 curl https://raw.githubusercontent.com/jpoon/aurora_deploy/master/ingress/docker.topach.service > /etc/systemd/system/docker.topach.service
 mkdir /etc/aurora
+curl https://raw.githubusercontent.com/jpoon/aurora_deploy/master/ingress/presence.conf > /etc/aurora/presence.conf
+#echo "USER=$1" >> $conf_file
+#echo "PASSWORD=$2" >> $conf_file
+#echo "PACH_IP=$3" >>$conf_file
+#echo "MONGO_URL=$4" >>$conf_file
+#echo "DATABASE_NAME=$5" >>$conf_file
+#echo "DBCOLLECTION=$6" >> $conf_file
 curl https://raw.githubusercontent.com/jpoon/aurora_deploy/master/ingress/topach.conf > /etc/aurora/topach.conf
 #echo "USER=$1" >> $conf_file
 #echo "PASSWORD=$2" >> $conf_file
@@ -9,5 +17,7 @@ curl https://raw.githubusercontent.com/jpoon/aurora_deploy/master/ingress/topach
 #echo "DATABASE_NAME=$5" >>$conf_file
 #echo "DBCOLLECTION=$6" >> $conf_file
 systemctl daemon-reload
+systemctl enable docker.presence.service
+systemctl start docker.presence.service
 systemctl enable docker.topach.service
 systemctl start docker.topach.service
