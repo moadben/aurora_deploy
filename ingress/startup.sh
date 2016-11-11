5328aea18000aa1035cf92ece2ab356ff18b7c5e
James Baker <jamesbak@microsoft.com>
2016-11-10 16:52:06 -0800

    Refactored ToPach service to share config with Presence service. Plumbed through port numbers for both.

--- ingress/startup.sh
+++ ingress/startup.sh
@@ -8,17 +8,21 @@
 #   $4: Full mongodb URL to write metadata to
 #   $5: Name of metadata database 
 #   $6: Name of metadata collection
+#   $7: Port number that Presence is listening on
+#   $8: Port number that ToPach is listening on
 curl https://raw.githubusercontent.com/jpoon/aurora_deploy/master/ingress/docker.topach.service > /etc/systemd/system/docker.topach.service
 
 # Emit our config file for ToPach service
 mkdir /etc/aurora
-conf_file=/etc/aurora/topach.conf
+conf_file=/etc/aurora/aurora.conf
 echo "DOCKERHUB_USER=$1" > $conf_file
 echo "DOCKERHUB_PASSWORD=$2" >> $conf_file
 echo "PACH_IP=$3" >>$conf_file
 echo "MONGO_URL=$4" >>$conf_file
-echo "DATABASE_NAME=$5" >>$conf_file
-echo "DBCOLLECTION=$6" >> $conf_file
+echo "DB_NAME=$5" >>$conf_file
+echo "DB_COLLECTION=$6" >> $conf_file
+echo "PRESENCE_PORT=${7:-6429}" >> $conf_file
+echo "TOPACH_PORT=${8:-4242}" >> $conf_file
 
 systemctl daemon-reload
 systemctl enable docker.topach.service
